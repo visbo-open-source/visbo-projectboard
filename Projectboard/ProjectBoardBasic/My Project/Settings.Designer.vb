73868c196159be449c5b99a186cf52971a3e07f9
Ute Rittinghaus-Koytek <ute.rittinghaus-koytek@visbo.de>
2015-11-04 13:41:02 +0100

    Korrektur von ProjektSteckbrief.xlsx

--- Projectboard/ProjectBoardBasic/My Project/Settings.Designer.vb
+++ Projectboard/ProjectBoardBasic/My Project/Settings.Designer.vb
@@ -65,7 +65,7 @@ Namespace My
         
         <Global.System.Configuration.ApplicationScopedSettingAttribute(),  _
          Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("bhtc")>  _
+         Global.System.Configuration.DefaultSettingValueAttribute("telairdemo")>  _
         Public ReadOnly Property mongoDBname() As String
             Get
                 Return CType(Me("mongoDBname"),String)
@@ -74,7 +74,8 @@ Namespace My
         
         <Global.System.Configuration.UserScopedSettingAttribute(),  _
          Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("\\KOYTEK-NAS\backup\Projekt-Tafel Folder\BHTC\")>  _
+         Global.System.Configuration.DefaultSettingValueAttribute("C:\Users\Ute\Documents\Visual Studio 2012\Projects\Projectboard\project-board-rep"& _ 
+            "o\Projectboard\Projectboard\bin\Debug\")>  _
         Public Property awinPath() As String
             Get
                 Return CType(Me("awinPath"),String)
