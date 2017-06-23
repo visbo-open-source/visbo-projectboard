680734b99c06d61944811ed14c4c57d0bbff5eca
Thomas <thomas.koytek@me.com>
2017-06-23 06:28:38 +0200

    Änderungen für ProjektStruktur-Auswahl jedoch nicht fertig

--- Projectboard/Projectboard/ThisWorkbook.vb
+++ Projectboard/Projectboard/ThisWorkbook.vb
@@ -442,14 +442,6 @@ Public Class ThisWorkbook
             .Visible = False
         End With
 
-        ' Ribbon ausblenden:  windowNames(4): Charts
-        ' jetzt die verbleibenden arrangieren ...
-
-        If appInstance.Version <> "14.0" Then
-            Call ThisWorkbook_WindowActivate(projectboardWindows(PTwindows.mptpr))
-            Call ThisWorkbook_WindowActivate(projectboardWindows(PTwindows.mptpf))
-        End If
-
 
         ' jetzt das Sheet Multiprojekt-Tafel aktivieren
         visboWorkbook.Worksheets.Item(arrWsNames(ptTables.MPT)).activate()
@@ -480,21 +472,44 @@ Public Class ThisWorkbook
   
 
     Private bIShrankTheRibbon As Boolean
-    Private Sub ThisWorkbook_WindowActivate(Wn As Microsoft.Office.Interop.Excel.Window) Handles Me.WindowActivate
-        If Wn.Caption = windowNames(4) Then
-            bIShrankTheRibbon = False
-            appInstance.ExecuteExcel4Macro("SHOW.TOOLBAR(" & Chr(34) & "Ribbon" & Chr(34) & ",False)")
-            bIShrankTheRibbon = True
-        End If
-    End Sub
+    'Private Sub ThisWorkbook_WindowActivate(Wn As Microsoft.Office.Interop.Excel.Window) Handles Me.WindowActivate
+    '    If Wn.Caption = windowNames(4) Then
+    '        bIShrankTheRibbon = False
+    '        appInstance.ExecuteExcel4Macro("SHOW.TOOLBAR(" & Chr(34) & "Ribbon" & Chr(34) & ",False)")
+    '        bIShrankTheRibbon = True
+    '    End If
+    'End Sub
 
-    Private Sub ThisWorkbook_WindowResize(Wn As Microsoft.Office.Interop.Excel.Window) Handles Me.WindowResize
-        'Dim anz As Integer = appInstance.Windows.Count
-        'Call MsgBox("Re-Size Window: " & CType(Wn.ActiveSheet, Excel.Worksheet).Name & "; Anzahl Fenster: " & anz)
+    'Private Sub ThisWorkbook_WindowResize(Wn As Microsoft.Office.Interop.Excel.Window) Handles Me.WindowResize
+    '    'Dim anz As Integer = appInstance.Windows.Count
+    '    'Call MsgBox("Re-Size Window: " & CType(Wn.ActiveSheet, Excel.Worksheet).Name & "; Anzahl Fenster: " & anz)
+    'End Sub
+
+    'Private Sub ThisWorkbook_WindowDeactivate(Wn As Microsoft.Office.Interop.Excel.Window) Handles Me.WindowDeactivate
+    '    'Dim anz As Integer = appInstance.Windows.Count
+    '    'Call MsgBox("De-Activate Window: " & CType(Wn.ActiveSheet, Excel.Worksheet).Name & "; Anzahl Fenster: " & anz)
+    'End Sub
+
+    Private Sub ThisWorkbook_WindowActivate(Wn As Microsoft.Office.Interop.Excel.Window) Handles Me.WindowActivate
+
+        ' Ribbon ausblenden:  windowNames(4): Charts
+        If appInstance.Version <> "14.0" Then
+            If Wn.Caption = windowNames(3) Then
+                bIShrankTheRibbon = False
+                appInstance.ExecuteExcel4Macro("SHOW.TOOLBAR(" & Chr(34) & "Ribbon" & Chr(34) & ",False)")
+                bIShrankTheRibbon = True
+            End If
+        End If
+       
     End Sub
 
     Private Sub ThisWorkbook_WindowDeactivate(Wn As Microsoft.Office.Interop.Excel.Window) Handles Me.WindowDeactivate
-        'Dim anz As Integer = appInstance.Windows.Count
-        'Call MsgBox("De-Activate Window: " & CType(Wn.ActiveSheet, Excel.Worksheet).Name & "; Anzahl Fenster: " & anz)
+        '    'Dim anz As Integer = appInstance.Windows.Count
+        '    'Call MsgBox("Re-Size Window: " & CType(Wn.ActiveSheet, Excel.Worksheet).Name & "; Anzahl Fenster: " & anz)
+    End Sub
+
+    Private Sub ThisWorkbook_WindowResize(Wn As Microsoft.Office.Interop.Excel.Window) Handles Me.WindowResize
+        '    'Dim anz As Integer = appInstance.Windows.Count
+        '    'Call MsgBox("De-Activate Window: " & CType(Wn.ActiveSheet, Excel.Worksheet).Name & "; Anzahl Fenster: " & anz)
     End Sub
 End Class
