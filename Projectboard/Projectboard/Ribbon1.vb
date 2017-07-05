2896f7f991a970f2672495fa56212350065d850f
Tom <thomas.koytek@visbo.de>
2017-07-05 14:58:12 +0200

    Auskommentieren der Invisible Setzung in PTMEShowChart

--- Projectboard/Projectboard/Ribbon1.vb
+++ Projectboard/Projectboard/Ribbon1.vb
@@ -8527,11 +8527,11 @@ Imports System.Windows
         End With
 
         'jetzt das Ursprungs-Window ausblenden ...
-        For Each tmpWindow As Excel.Window In visboWorkbook.Windows
-            If (CStr(tmpWindow.Caption) <> windowNames(4)) And (CStr(tmpWindow.Caption) <> windowNames(1)) Then
-                tmpWindow.Visible = False
-            End If
-        Next
+        'For Each tmpWindow As Excel.Window In visboWorkbook.Windows
+        '    If (CStr(tmpWindow.Caption) <> windowNames(4)) And (CStr(tmpWindow.Caption) <> windowNames(1)) Then
+        '        tmpWindow.Visible = False
+        '    End If
+        'Next
 
 
         visboWorkbook.Windows.Arrange(Excel.XlArrangeStyle.xlArrangeStyleHorizontal)
