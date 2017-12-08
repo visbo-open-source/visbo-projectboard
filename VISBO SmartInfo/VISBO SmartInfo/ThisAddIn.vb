dcdf0676bd71fa358d0b875288b0f54bfbbbf57f
THINK-PHILIPP\Philipp <philipp.koytek@me.com>
2017-12-08 19:54:02 +0100

    bugfix: search und properties panes werden fuer jede Powerpoint-Präsentation neu initialisiert

--- VISBO SmartInfo/VISBO SmartInfo/ThisAddIn.vb
+++ VISBO SmartInfo/VISBO SmartInfo/ThisAddIn.vb
@@ -1,8 +1,10 @@
-﻿Public Class ThisAddIn
+﻿Imports Microsoft.Office.Interop.PowerPoint
+
+Public Class ThisAddIn
 
     'Private pane As ucInfo
     'Private ucSearchView As ucSearch
-    Private WithEvents thePane As Microsoft.Office.Tools.CustomTaskPane
+    'Private WithEvents thePane As Microsoft.Office.Tools.CustomTaskPane
     'Private WithEvents searchPane As Microsoft.Office.Tools.CustomTaskPane
 
     Private Sub ThisAddIn_Disposed(sender As Object, e As EventArgs) Handles Me.Disposed
@@ -14,6 +16,34 @@
     Private Sub ThisAddIn_Startup() Handles Me.Startup
         pptAPP = Application
 
+        trafficLightColors(0) = PowerPoint.XlRgbColor.rgbGray
+        trafficLightColors(1) = PowerPoint.XlRgbColor.rgbGreen
+        trafficLightColors(2) = PowerPoint.XlRgbColor.rgbYellow
+        trafficLightColors(3) = PowerPoint.XlRgbColor.rgbRed
+        trafficLightColors(4) = PowerPoint.XlRgbColor.rgbWhite
+
+        showTrafficLights(0) = False
+        showTrafficLights(1) = False
+        showTrafficLights(2) = False
+        showTrafficLights(3) = False
+        showTrafficLights(4) = False
+
+        ' muss hier noch auf andere Art und Weise bestimmt werden 
+        englishLanguage = True
+
+
+    End Sub
+
+
+
+
+    Private Sub ThisAddIn_Shutdown() Handles Me.Shutdown
+        If VisboProtected Then
+            Call makeVisboShapesVisible(False)
+        End If
+    End Sub
+
+    Private Sub Application_OpenPresentation(Pres As Presentation) Handles Application.AfterPresentationOpen, Application.AfterNewPresentation
         ' Definition des Search-Pane
         ucSearchView = New ucSearch
         If englishLanguage Then
@@ -48,8 +78,8 @@
             .Visible = False
         End With
 
-        'pane = New ucInfo
-        'thePane = Me.CustomTaskPanes.Add(pane, "Info")
+        'Pane = New ucInfo
+        'thePane = Me.CustomTaskPanes.Add(Pane, "Info")
         'With thePane
         '    .DockPosition = Office.MsoCTPDockPosition.msoCTPDockPositionFloating
         '    .Height = 500
@@ -59,32 +89,5 @@
         '    .Visible = True
         'End With
 
-
-        trafficLightColors(0) = PowerPoint.XlRgbColor.rgbGray
-        trafficLightColors(1) = PowerPoint.XlRgbColor.rgbGreen
-        trafficLightColors(2) = PowerPoint.XlRgbColor.rgbYellow
-        trafficLightColors(3) = PowerPoint.XlRgbColor.rgbRed
-        trafficLightColors(4) = PowerPoint.XlRgbColor.rgbWhite
-
-        showTrafficLights(0) = False
-        showTrafficLights(1) = False
-        showTrafficLights(2) = False
-        showTrafficLights(3) = False
-        showTrafficLights(4) = False
-
-        ' muss hier noch auf andere Art und Weise bestimmt werden 
-        englishLanguage = True
-
-
     End Sub
-
-
-
-    Private Sub ThisAddIn_Shutdown() Handles Me.Shutdown
-        If VisboProtected Then
-            Call makeVisboShapesVisible(False)
-        End If
-    End Sub
-
-
 End Class
