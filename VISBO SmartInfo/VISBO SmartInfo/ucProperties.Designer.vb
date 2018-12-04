c99a923e3f1b9194715dd925b7b3035fe25242e7
LAPTOP-BLVBC31D\Tom <ute.rittinghaus-koytek@visbo.de>
2018-12-04 12:56:27 +0100

    Formulare in smartInfo wieder auf dpi statt font gestellt ...

--- VISBO SmartInfo/VISBO SmartInfo/ucProperties.Designer.vb
+++ VISBO SmartInfo/VISBO SmartInfo/ucProperties.Designer.vb
@@ -299,8 +299,8 @@ Partial Class ucProperties
         '
         'ucProperties
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(96.0!, 96.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
         Me.AutoScroll = True
         Me.BackColor = System.Drawing.SystemColors.ControlLightLight
         Me.Controls.Add(Me.eleAmpelText)
