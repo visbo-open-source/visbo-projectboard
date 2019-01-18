cc3738e49b6258c0f2ebd0cd15e40fac10b34fa7
LAPTOP-BLVBC31D\Tom <thomas.koytek@visbo.de>
2019-01-18 09:39:07 +0100

    Tikmestamps werden jetzt beim Löschen auch bei Rolle Portfolio Mgr richtig angezeigt

--- Projectboard/ProjectBoardBasic/frmHierarchySelection.vb
+++ Projectboard/ProjectBoardBasic/frmHierarchySelection.vb
@@ -1796,7 +1796,7 @@ Public Class frmHierarchySelection
                     Dim nodelist As New SortedList(Of Integer, Double)
                     Try
                         Dim teamID As Integer
-                        nodelist = RoleDefinitions.getRoleDefByIDKennung(CInt(node.Name), teamID).getSubRoleIDs
+                        nodelist = RoleDefinitions.getRoleDefByIDKennung(node.Name, teamID).getSubRoleIDs
                         anzChilds = nodelist.Count
                     Catch ex As Exception
                         anzChilds = 0
@@ -5271,11 +5271,13 @@ Public Class frmHierarchySelection
         ElseIf currentRole.getTeamIDs.Count > 0 And CType(parentNode.Tag, clsNodeRoleTag).isTeam Then
 
             nrTag = New clsNodeRoleTag
+            Dim teamID As Integer
+            Dim parentID As Integer = RoleDefinitions.parseRoleNameID(parentNode.Name, teamID)
             With nrTag
                 .isTeam = False
                 .isTeamMember = True
-                .membershipID = CInt(parentNode.Name)
-                .membershipPrz = RoleDefinitions.getMembershipPrz(CInt(parentNode.Name), currentRoleUid)
+                .membershipID = parentID
+                .membershipPrz = RoleDefinitions.getMembershipPrz(parentID, currentRoleUid)
             End With
         End If
 
