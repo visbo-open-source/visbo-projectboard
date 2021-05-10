872fd6c78347940592ff59d2bdd637b1ef57b2f9
Ute <ute.rittinghaus-koytek@visbo.de>
2021-05-10 15:36:11 +0200

    settings für den Test angepasst

--- VISBO SmartInfo/VISBO SmartInfo/Ribbon1.vb
+++ VISBO SmartInfo/VISBO SmartInfo/Ribbon1.vb
@@ -200,13 +200,13 @@ Public Class Ribbon1
 
     Private Sub activateInfo_Click(sender As Object, e As RibbonControlEventArgs) Handles activateInfo.Click
         Try
-
-            If propertiesPane.Visible Then
-                propertiesPane.Visible = False
-            Else
-                propertiesPane.Visible = True
+            If Not IsNothing(propertiesPane) Then
+                If propertiesPane.Visible Then
+                    propertiesPane.Visible = False
+                Else
+                    propertiesPane.Visible = True
+                End If
             End If
-
 
         Catch ex As Exception
             Call MsgBox(ex.StackTrace)
