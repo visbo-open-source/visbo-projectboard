6f359d685f09c6f018ed47210fff8f49a6abe540
AzureAD\UteRittinghaus-Koyte <ute.rittinghaus-koytek@visbo.de>
2022-12-09 15:02:08 +0100

    Projectboard: new classes for uploaded reportmessages

---
+++ Projectboard/ProjectBoardDefinitions/clsReportMessage.vb
@@ -0,0 +1,12 @@
+﻿Public Class clsReportMessage
+    Public Property german As String
+    Public Property english As String
+    Public Property french As String
+    Public Property spanish As String
+    Sub New()
+        _german = "undefiniert"
+        _english = "undefined"
+        _french = "indéfini"
+        _spanish = "indefinido"
+    End Sub
+End Class
